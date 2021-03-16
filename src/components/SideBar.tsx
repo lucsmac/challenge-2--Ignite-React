aeb63c1154e9bc9a1e1b26218eb7828c939b9336
Lucas Macedo <lucasmdc57@gmail.com>
2021-03-16 20:43:28 -0300

    componetizing

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => void;
+  selectedGenre: number;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenre }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenre === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
